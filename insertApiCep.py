e4890157677677755cd187661897bb832fe913ec
Gabriel <gabriel23753@gmail.com>
2025-06-26 22:49:48 -0300

    atx insertCep

--- insertApiCep.py
+++ insertApiCep.py
@@ -1,15 +1,30 @@
 import time
 import requests
-import pandas as pd
 
-def consultaCep(cep):
+ceps_cache = {} 
+
+def consultaCepComCache(cep):
+    cep = cep.strip().replace('.', '').replace('-', '')
+    if len(cep) != 8 or not cep.isdigit():
+        return "CEP inválido."
+    
+
+    if cep in ceps_cache:
+        print(f"Usando cache para CEP {cep}")
+        return ceps_cache[cep]
+
     url = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(url)
-
+    
     if response.status_code == 200:
         dados = response.json()
         if "erro" in dados:
-            return "CEP não encontrado."
-        return dados
+            ceps_cache[cep] = "CEP não encontrado."
+        else:
+            ceps_cache[cep] = dados
     else:
-        return f"Erro na consulta: {response.status_code}"
+        ceps_cache[cep] = f"Erro na consulta: {response.status_code}"
+
+    time.sleep(0.3)
+    
+    return ceps_cache[cep]
